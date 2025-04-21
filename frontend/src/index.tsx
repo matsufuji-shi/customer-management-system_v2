7b75dd141c92c07519bc23659277682c0b17f8a2
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-21 11:05:10 +0900

    エラー解消

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Appコンポーネントをルート要素にレンダリング
-ReactDOM.render(
-    <App />,
-  document.getElementById('root') // HTMLファイル内のid="root"の要素に描画
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
